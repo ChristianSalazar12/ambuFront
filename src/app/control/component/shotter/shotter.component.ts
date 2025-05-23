77a05e05ac866bf664e49e627395f4e0b9198d7c
Carmen Ortega <carmenortegarojas@gmail.com>
2025-05-23 04:51:49 -0500

    Creación de los diferentes çomponentes de 'Control'

---
+++ src/app/control/component/shotter/shotter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shotter',
+  imports: [],
+  templateUrl: './shotter.component.html',
+  styleUrl: './shotter.component.css'
+})
+export class ShotterComponent {
+
+}
