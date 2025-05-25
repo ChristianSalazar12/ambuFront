2df14eb1896a8134405ba3333958018cf5194f90
Carmen Ortega <carmenortegarojas@gmail.com>
2025-05-24 22:01:40 -0500

    Creación de ruta para el StaffComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './control/features/components/home/home.component';
+import { StaffComponent } from './control/features/components/staff/staff.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'staff', // Ruta para el componente Staff
+        component: StaffComponent
     },
     { path: '**', redirectTo: '' }
 ];
