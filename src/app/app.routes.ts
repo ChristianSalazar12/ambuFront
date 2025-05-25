c252b2bd8ba86d0291508eda76b39144dc44abb6
Carmen Ortega <carmenortegarojas@gmail.com>
2025-05-24 22:13:52 -0500

    Revert "Merge pull request #1 from ChristianSalazar12/feature/staff"
    This reverts commit 358c16b16084a70a0f04f52fee3e549945b66c3a, reversing
    changes made to 817362f6ac5295d50cec24c249c91148e82970bb.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './control/features/components/home/home.component';
 
-export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    { path: '**', redirectTo: '' }
-];
+export const routes: Routes = [];
